abf4b9a4084056cfcc03f61b55a83d1eff9f471d
Adrian Medina <medinalarry96@gmail.com>
2020-09-17 12:38:48 +0200

    Aggiunta commentari

--- MyCourse/Startup.cs
+++ MyCourse/Startup.cs
@@ -16,7 +16,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2); // indica la versione da utilizzare
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
