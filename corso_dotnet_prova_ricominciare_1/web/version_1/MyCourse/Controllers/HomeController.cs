fd2234cf18703f5560ca458d6c7dbd4e7dc22ab6
Adrian Medina <medinalarry96@gmail.com>
2020-10-25 23:14:33 +0100

    Aggiunta vista index al home controller

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Controllers/HomeController.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ namespace MyCourse.Controllers
     public class HomeController : Controller{
         public IActionResult Index()
         {
-            return Content("Home");
+            return View();
         }
 
     }
