e00ee9e3f8e4d8fe44e1069353f6d6153197672b
Adrian Medina <medinalarry96@gmail.com>
2020-10-27 09:46:00 +0100

    Creare il servizio corsi

---
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/Services/Application/CourseService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using MyCourse.ViewModels;
+
+namespace MyCourse.Models.Services.Application
+{
+    public class CourseService
+    {
+        public List<CourseViewModel> getCourse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
