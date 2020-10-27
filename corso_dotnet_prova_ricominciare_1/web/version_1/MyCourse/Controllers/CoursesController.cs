517fcd049c954ac4ecb4ba51bc73c30148fab1ff
Adrian Medina <medinalarry96@gmail.com>
2020-10-27 17:48:40 +0100

    Aggiornando il percorso using

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Controllers/CoursesController.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Controllers/CoursesController.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using MyCourse.Models.Services.Application;
-using MyCourse.ViewModels;
+using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Controllers
 {
