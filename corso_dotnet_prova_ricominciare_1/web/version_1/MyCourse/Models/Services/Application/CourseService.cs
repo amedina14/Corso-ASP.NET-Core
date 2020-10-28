d4780282e2d7af112aa76ba6170710aaaddaa72a
Adrian Medina <medinalarry96@gmail.com>
2020-10-28 22:24:22 +0100

    Ottenere il singolo corso dal servizio

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/Services/Application/CourseService.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/Services/Application/CourseService.cs
@@ -8,19 +8,19 @@ namespace MyCourse.Models.Services.Application
 {
     public class CourseService
     {
-        public List<CourseViewModel> getCourse()
+        public List<CourseViewModel> getCourses()
         {
             var courseList = new List<CourseViewModel>();
             var rand = new Random();
-            for (int i = 0; i < 20; i++)
+            for (int i = 1; i <= 20; i++)
             {
-                var price = Convert.ToDecimal(rand.NextDouble() * 10 + 10);
+                var price = Convert.ToDecimal(rand.NextDouble() * 10 + 120);
                 var course = new CourseViewModel
                 {
                     Id = i,
                     Title = $"Corso {i}",
                     FullPrice = new Money(Currency.EUR, price),
-                    CurrentPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price : price - 1),
+                    CurrentPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price : price - 110),
                     Rating = (double)Decimal.Round((decimal)(rand.NextDouble() * 5.0), 1),
                     Author = "Name Surname",
                     ImagePath = "~/img/bug.png"
@@ -28,6 +28,32 @@ namespace MyCourse.Models.Services.Application
                 courseList.Add(course);
             }
             return courseList;
+        }
+
+        public CourseDetailViewModel GetCourse(int id){
+            var rand = new Random();
+            var price = Convert.ToDecimal(rand.NextDouble() * 10 + 120);
+            var corso = new CourseDetailViewModel(){
+                Id = id,
+                Title = $"Corso {id}",
+                FullPrice = new Money(Currency.EUR, price),
+                CurrentPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price : price - 110),
+                Rating = rand.Next(10, 50) / 10.0,
+                Description = $"Description {id}",
+                Lezioni = new List<CourseLessonViewModel>(),
+                Author = "Nome Cognome",
+                ImagePath = "~/img/bug.png",
+            };
+            
+            for(var i = 1 ; i <= 5; i++){
+                var lezione = new CourseLessonViewModel(){
+                    TitleLesson = $"Lezione {i}",
+                    Duration = TimeSpan.FromSeconds(rand.Next(40,90))
+                };
+                corso.Lezioni.Add(lezione);
+            }
+
+            return corso;
         }
     }
 }
