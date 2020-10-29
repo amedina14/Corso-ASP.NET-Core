c55fe0fb5deaa02fad4f5245a5bddb8de9289269
Adrian Medina <medinalarry96@gmail.com>
2020-10-29 11:32:25 +0100

    Scambiare dati con le view dal action HomeCntrller

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Controllers/HomeController.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace MyCourse.Controllers
     public class HomeController : Controller{
         public IActionResult Index()
         {
+            ViewBag.title = "Home";
             return View();
         }
 
