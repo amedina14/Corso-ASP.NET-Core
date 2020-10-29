78dc804ae24cb55ffa1c164124146652793eeb8d
Adrian Medina <medinalarry96@gmail.com>
2020-10-29 20:18:50 +0100

    DI a CourseController. Debolmente accoppiato

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Controllers/CoursesController.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Controllers/CoursesController.cs
@@ -9,23 +9,25 @@ namespace MyCourse.Controllers
     {
 
         // Debolmente accopiato: DI
-        private readonly CourseService corsoServizio;
+        private readonly ICourseService corsoServizio;
 
-        public CoursesController(CourseService corsoServizio){
+        public CoursesController(ICourseService corsoServizio){
             this.corsoServizio = corsoServizio;
         }
 
         public IActionResult index(){
-            // Fortemente accopiato
-            //CourseService corsoServizio = new CourseService();
-            List<CourseViewModel> corsi = corsoServizio.getCourses();
-            //ViewData["title"] = "Catalogo dei corsi";
+            /**
+            * Fortemente accoppiato
+            * CourseService corsoServizio = new CourseService();
+            * ViewData["title"] = "Catalogo dei corsi";
+            */
             ViewBag.title = "Catalogo dei corsi";
+            List<CourseViewModel> corsi = corsoServizio.GetCourses();
             return View(corsi);
         }
 
         public IActionResult Detail(int id){
-            var corsoServizio = new CourseService();
+            //var corsoServizio = new CourseService();
             CourseDetailViewModel detailViewModel = corsoServizio.GetCourse(id);
             ViewBag.title = detailViewModel.Title;
             // Se non passo l'oggetto alla view, lancia NullReferenceException
