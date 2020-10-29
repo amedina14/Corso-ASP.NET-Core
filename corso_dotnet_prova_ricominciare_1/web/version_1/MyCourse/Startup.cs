0f87d9a63fe7f1ce30bc6cb883811048b13d0238
Adrian Medina <medinalarry96@gmail.com>
2020-10-29 16:07:12 +0100

    Aggiunta del servizio nelle configurazioni middlew

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Startup.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using MyCourse.Models.Services.Application;
 
 namespace MyCourse
 {
@@ -17,6 +18,7 @@ namespace MyCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<CourseService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
