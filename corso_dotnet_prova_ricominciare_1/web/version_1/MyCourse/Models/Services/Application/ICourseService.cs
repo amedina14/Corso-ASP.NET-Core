4241ea7883a537aca8e2ad15ebf8cf1d239529c1
Adrian Medina <medinalarry96@gmail.com>
2020-10-29 20:07:03 +0100

    Creazione della classe Interface

---
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/Services/Application/ICourseService.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using MyCourse.Models.ViewModels;
+
+namespace MyCourse.Models.Services.Application{
+    public interface ICourseService
+    {
+        // In questa classe si implementano i metodi dell'interfaccia.
+        
+        // Si consiglia scrivere prima la classe interfaccia e poi la sua implementazione concreta.
+        // in questa maniera ci si concentra su come deve essere realizzato il servizio.
+
+        // Firma: tipo del metodo, nome del metodo, ed eventualmente i parametri.
+        // Modificatore public implicito se non scritto. (Convenzione)
+        List<CourseViewModel> GetCourses();
+
+        CourseDetailViewModel GetCourse(int id);
+    }
+}
