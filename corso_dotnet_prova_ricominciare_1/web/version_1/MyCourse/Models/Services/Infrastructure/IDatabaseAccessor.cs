58e12f9d4dcc10e93d0f0f81b9b1ba336fabf507
Adrian Medina <medinalarry96@gmail.com>
2020-10-30 19:17:27 +0100

    Implementazione nuova interfaccia per accesso dati

---
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/Services/Infrastructure/IDatabaseAccessor.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using MyCourse.Models.ViewModels;
+
+namespace MyCourse.Models.Services.Infrastructure{
+    public interface IDatabaseAccessor
+    {
+        DataSet Query(string query);
+    }
+}
