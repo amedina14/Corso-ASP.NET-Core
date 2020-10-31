f1415700d03da08cecee0786767f91f7838257e9
Adrian Medina <medinalarry96@gmail.com>
2020-10-31 14:41:15 +0100

    Usare classi ADO.NET per accedere al DB

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Data;
+using Microsoft.Data.Sqlite;
 using MyCourse.Models.Services.Application;
 
 namespace MyCourse.Models.Services.Infrastructure{
@@ -7,7 +8,48 @@ namespace MyCourse.Models.Services.Infrastructure{
     public class SqliteDatabaseAccessor : IDatabaseAccessor{
 
         public DataSet Query(string query){
-            throw new NotImplementedException();            
+
+            /* 
+                Il blocco using serve a distruggere gli oggetti correttamente (come la connessione),
+                anche quando si verifica un'eccezione, laddove si implementi una classe IDisposable.
+            */
+            using(var conn = new SqliteConnection("Data Source=Data/MyCourse.db"))
+            {
+                conn.Open();
+                using(var cmd = new SqliteCommand(query, conn))
+                {
+                    // A questo punto abbiamo trovato i risultati. Ora li leggiamo uno ad uno.
+                    using(SqliteDataReader reader = cmd.ExecuteReader())
+                    {
+                        // E li passiamo alle classi disconnesse: DataSet, DataTable.
+                        // Crea DataSet
+                        var dataSet = new DataSet();
+
+                        //dataSet.EnforceConstraints = false;
+
+                        // Crea DataTable
+                        var dataTable = new DataTable();
+
+                        // Aggiunge la tabella al set
+                        dataSet.Tables.Add(dataTable);
+
+                        /*
+                        while(reader.Read()){
+                            string title = (string) reader["Title"]; 
+                        }
+                        */
+
+                        // Legge i registri
+                        dataTable.Load(reader);
+
+                        return dataSet;
+                    }
+                }                
+            }
+            /*
+                Attenzione: Usando conn.Dispose(); 
+                Se si produce un errore la connessione resta aperta.
+            */
         }
     }
 }
