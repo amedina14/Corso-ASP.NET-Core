63a18d0c693c3beab2242063b5f34f3303014d21
Adrian Medina <medinalarry96@gmail.com>
2020-11-03 16:46:03 +0100

    Mappatura dei valori del dettaglio corso

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/ViewModels/CourseDetailViewModel.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/ViewModels/CourseDetailViewModel.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using MyCourse.Models.Enums;
+using MyCourse.Models.ValueTypes;
 
 namespace MyCourse.Models.ViewModels
 {
@@ -9,6 +12,31 @@ namespace MyCourse.Models.ViewModels
         public List<CourseLessonViewModel> Lezioni { get; set; }
         public TimeSpan TotalCourseDuration { 
             get => TimeSpan.FromSeconds(Lezioni?.Sum(l => l.Duration.TotalSeconds) ?? 0);
+        }
+
+        public static new CourseDetailViewModel FromDataRow(DataRow courseRow)
+        {
+            var courseDetailViewModel = new CourseDetailViewModel 
+            {
+                Title = Convert.ToString(courseRow["Title"]),
+                Description = Convert.ToString(courseRow["Description"]),
+                ImagePath = Convert.ToString(courseRow["ImagePath"]),
+                Author = Convert.ToString(courseRow["Author"]),
+                Rating = Convert.ToDouble(courseRow["Rating"]),
+                FullPrice = new Money(
+                    Enum.Parse<Currency>(Convert.ToString(courseRow["FullPrice_Currency"])),
+                    Convert.ToDecimal(courseRow["FullPrice_Amount"])
+                ),
+                CurrentPrice = new Money(
+                    Enum.Parse<Currency>(Convert.ToString(courseRow["CurrentPrice_Currency"])),
+                    Convert.ToDecimal(courseRow["CurrentPrice_Amount"])
+                ),
+                Id = Convert.ToInt32(courseRow["Id"]),
+                Lezioni = new List<CourseLessonViewModel>()
+            };
+            
+            return courseDetailViewModel;
+            //throw new NotImplementedException();
         }
     }
 }
