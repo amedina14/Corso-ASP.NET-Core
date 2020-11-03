4336571c5995308880b33fe105a0c241bc17df09
Adrian Medina <medinalarry96@gmail.com>
2020-11-03 16:43:51 +0100

    Mappatura del modello per le lezioni del corso

--- corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/ViewModels/CourseLessonViewModel.cs
+++ corso_dotnet_prova_ricominciare_1/web/version_1/MyCourse/Models/ViewModels/CourseLessonViewModel.cs
@@ -1,10 +1,25 @@
 using System;
+using System.Data;
 
 namespace MyCourse.Models.ViewModels
 {
     public class CourseLessonViewModel
     {
+        public int Id { get; set; }
         public string TitleLesson { get; set; }
+        public string Description { get; set; }
         public TimeSpan Duration { get; set; }
+
+        public static CourseLessonViewModel FromDataRow(DataRow lessonRow)
+        {
+            var courseLessonViewModel = new CourseLessonViewModel {
+                Id = Convert.ToInt32(lessonRow["Id"]),
+                TitleLesson = Convert.ToString(lessonRow["Title"]),
+                Description = Convert.ToString(lessonRow["Description"]),
+                Duration = TimeSpan.Parse(Convert.ToString(lessonRow["Duration"])),
+            };
+
+            return courseLessonViewModel;
+        }
     }
 }
